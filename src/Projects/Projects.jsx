aa25f6e72d8f8db391f80efb845d57e59456ad87
Sarvin Shrivastava <sarvin5124@gmail.com>
2025-02-10 00:54:00 +0530

    Optimized the side nav bar for all the routes

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -38,8 +38,8 @@ function Projects() {
 
   return (
     <div className="flex flex-row h-screen">
-      {/* side navigation bar */}
-      {/* md+ */}
+      {/* Side Navigation Bar */}
+      {/* For md+ screens */}
       <div
         className={`bg-neutral-800 hidden md:block absolute top-0 left-0 h-full ${
           isExpanded ? "w-48 left-0" : "w-18 -left-48"
@@ -54,96 +54,39 @@ function Projects() {
         </div>
         <div
           style={{ top: `${pos_route}px`, height: `${spacing_route}px` }}
-          className={`flex flex-col left-4.5 relative justify-between`}
+          className="flex flex-col left-4.5 relative justify-between"
         >
-          <Link to="/" className="flex items-center relative">
-            <div
-              className="w-fit hover:scale-125 duration-300"
-              onMouseEnter={() => handleMouseEnter("home-outline")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <ion-icon name={getIconName("home-outline")} size="large" />
-            </div>
-            <span
-              className={`absolute left-10 text-white transition-opacity duration-300 text-lg ${
-                isExpanded ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              Home
-            </span>
-          </Link>
-          <Link to="/about" className="flex items-center relative">
-            <div
-              className="w-fit hover:scale-125 duration-300"
-              onMouseEnter={() => handleMouseEnter("person-outline")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <ion-icon name={getIconName("person-outline")} size="large" />
-            </div>
-            <span
-              className={`absolute left-10 text-white transition-opacity duration-300 text-lg ${
-                isExpanded ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              About
-            </span>
-          </Link>
-          <Link to="/achievements" className="flex items-center relative">
-            <div
-              className="w-fit hover:scale-125 duration-300"
-              onMouseEnter={() => handleMouseEnter("trophy-outline")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <ion-icon name={getIconName("trophy-outline")} size="large" />
-            </div>
-            <span
-              className={`absolute left-10 text-white transition-opacity duration-300 text-lg ${
-                isExpanded ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              Achievements
-            </span>
-          </Link>
-          <Link to="/projects" className="flex items-center relative">
-            <div
-              className="w-fit hover:scale-125 duration-300"
-              onMouseEnter={() => handleMouseEnter("folder-outline")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <ion-icon name={getIconName("folder-outline")} size="large" />
-            </div>
-            <span
-              className={`absolute left-10 text-white transition-opacity duration-300 text-lg ${
-                isExpanded ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              Projects
-            </span>
-          </Link>
-          <Link to="/contact" className="flex items-center relative">
-            <div
-              className="w-7 hover:scale-125 duration-300"
-              onMouseEnter={() => handleMouseEnter("chatbubble-outline")}
-              onMouseLeave={handleMouseLeave}
-            >
-              <ion-icon name={getIconName("chatbubble-outline")} size="large" />
-            </div>
-            <span
-              className={`absolute left-10 text-white transition-opacity duration-300 text-lg ${
-                isExpanded ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              Contact
-            </span>
-          </Link>
+          {[
+            { to: "/", icon: "home-outline", label: "Home" },
+            { to: "/about", icon: "person-outline", label: "About" },
+            {
+              to: "/achievements",
+              icon: "trophy-outline",
+              label: "Achievements",
+            },
+            { to: "/projects", icon: "folder-outline", label: "Projects" },
+            { to: "/contact", icon: "chatbubble-outline", label: "Contact" },
+          ].map(({ to, icon, label }) => (
+            <Link to={to} className="flex items-center relative" key={label}>
+              <div
+                className="w-fit hover:scale-125 duration-300"
+                onMouseEnter={() => handleMouseEnter(icon)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <ion-icon name={getIconName(icon)} size="large" />
+              </div>
+              <span
+                className={`absolute left-10 text-white transition-opacity duration-300 text-lg ${
+                  isExpanded ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                {label}
+              </span>
+            </Link>
+          ))}
         </div>
-        {/* <img
-          src="./night-day.png"
-          className="w-7 mt-15 hover:scale-125 duration-300 absolute left-5 bottom-24"
-          style={{ bottom: `${pos_ham}px` }}
-        /> */}
       </div>
-      {/* sm */}
+      {/* For sm screens */}
       <div
         className={`bg-neutral-800 md:hidden absolute top-0 left-0 h-full ${
           isExpanded ? "w-1/2" : "w-0"
@@ -165,63 +108,29 @@ function Projects() {
         </div>
         <div
           style={{ top: `${pos_route}px`, height: `${spacing_route}px` }}
-          className={`flex flex-col left-4.5 relative justify-between`}
+          className="flex flex-col left-4.5 relative justify-between"
         >
-          <Link
-            to="/"
-            className={`flex items-center transition-opacity duration-300 ${
-              isExpanded ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <ion-icon name="home" size="large" />
-            <span className="absolute left-10 text-white text-sm">Home</span>
-          </Link>
-          <Link
-            to="/about"
-            className={`flex items-center transition-opacity duration-300 ${
-              isExpanded ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <ion-icon name="person" size="large" />
-            <span className="absolute left-10 text-white text-sm">About</span>
-          </Link>
-          <Link
-            to="/achievements"
-            className={`flex items-center transition-opacity duration-300 ${
-              isExpanded ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <ion-icon name="trophy" size="large" />
-            <span className="absolute left-10 text-white text-sm">
-              Achievements
-            </span>
-          </Link>
-          <Link
-            to="/projects"
-            className={`flex items-center transition-opacity duration-300 ${
-              isExpanded ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <ion-icon name="folder" size="large" />
-            <span className="absolute left-10 text-white text-sm">
-              Projects
-            </span>
-          </Link>
-          <Link
-            to="/contact"
-            className={`flex items-center transition-opacity duration-300 ${
-              isExpanded ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <ion-icon name="chatbubble" size="large" />
-            <span className="absolute left-10 text-white text-sm">Contact</span>
-          </Link>
+          {[
+            { to: "/", icon: "home", label: "Home" },
+            { to: "/about", icon: "person", label: "About" },
+            { to: "/achievements", icon: "trophy", label: "Achievements" },
+            { to: "/projects", icon: "folder", label: "Projects" },
+            { to: "/contact", icon: "chatbubble", label: "Contact" },
+          ].map(({ to, icon, label }) => (
+            <Link
+              to={to}
+              className={`flex items-center transition-opacity duration-300 ${
+                isExpanded ? "opacity-100" : "opacity-0"
+              }`}
+              key={label}
+            >
+              <ion-icon name={icon} size="large" />
+              <span className="absolute left-10 text-white text-sm">
+                {label}
+              </span>
+            </Link>
+          ))}
         </div>
-        {/* <img
-          src="./night-day.png"
-          className="w-7 mt-15 hover:scale-125 duration-300 absolute left-5 bottom-24"
-          style={{ bottom: `${pos_ham}px` }}
-        /> */}
       </div>
       {/* projects page content */}
       <div className="justify-center items-center flex flex-col mx-auto">
